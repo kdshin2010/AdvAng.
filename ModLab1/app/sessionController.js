8efa481378548672049f70aa0bafe59d57c04cb4
Kyle Shin <kdshin2010@gmail.com>
2016-11-22 21:03:26 -0800

    mod1lab1 finished

--- ModLab1/app/sessionController.js
+++ ModLab1/app/sessionController.js
@@ -1,5 +1,33 @@
-angular.module('app').controller('sessionController', [
-    function () {
-        var vm = this;
-    }
-]);
+(function() {
+    'use strict';
+    angular
+        .module('app')
+        .controller('sessionController', sessionController);
+
+        sessionController.$inject = ['sessionService'];
+
+        function sessionController(sessionService) {
+            var vm = this;
+
+            vm.getServiceSession = function() {
+                vm.model = {
+                    name: sessionService.get('name'),
+                    nickname: sessionService.get('nickname'),
+                    status: 'Retrieved by service on ' + new Date()
+                }
+            }
+
+            
+            vm.setServiceSessions = function() {
+                sessionService.save('name', vm.model.name);
+                sessionService.save('nickname', vm.model.nickname);
+                vm.getServiceSession();
+            }
+
+            vm.clearServiceSession = function() {
+                sessionService.clear();
+                vm.getServiceSession();
+
+            }
+        }
+})();
